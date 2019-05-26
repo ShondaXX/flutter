55ab960cdc854042600e9572f861cebfa86080b0
xh <xhconceit@163.com>
2019-05-26 15:31:20 +0800

    ListView.builder:列表视图

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import './model/post.dart';
 
 void main() => runApp(App());
 
@@ -7,18 +8,34 @@ class App extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       //Material 风格的小部件
-      home: Scaffold(
-        //脚手架小部件
-        appBar: AppBar(
-          //顶部工具栏小部件
-          title: Text("hello"), //标题
-          elevation: 0.0,//顶部工具栏阴影
-        ),
-        body: Hello(),
+      home: Home(),
+      theme: ThemeData(
+          //主题颜色小部件
+          primarySwatch: Colors.yellow //主要颜色
+          ),
+    );
+  }
+}
+
+class Home extends StatelessWidget {
+  Widget _listItemBuilder(BuildContext context,int index){
+    return Text(posts[index].title);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      //脚手架小部件
+      appBar: AppBar(
+        //顶部工具栏小部件
+        title: Text("hello"), //标题
+        elevation: 0.0, //顶部工具栏阴影
       ),
-      theme: ThemeData(//主题颜色小部件
-        primarySwatch: Colors.yellow//主要颜色
+      body: ListView.builder(//列表视图小部件
+        itemCount: posts.length,//列表数量
+        itemBuilder: _listItemBuilder,//列表视图项目
       ),
+      //Hello()
     );
   }
 }
---
+++ lib/model/post.dart
@@ -0,0 +1,113 @@
+class Post {
+  const Post({
+    this.title,
+    this.author,
+    this.imageUrl,
+  });
+  final String title;
+  final String author;
+  final String imageUrl;
+}
+
+final List<Post> posts = [
+  Post(
+      title: "lijinshanmx",
+      author: "2019-05-05",
+      imageUrl:
+          "http://ww1.sinaimg.cn/large/0065oQSqly1g2pquqlp0nj30n00yiq8u.jpg"),
+  Post(
+      title: "lijinshanmx",
+      author: "2019-04-27",
+      imageUrl:
+          "https://ww1.sinaimg.cn/large/0065oQSqly1g2hekfwnd7j30sg0x4djy.jpg"),
+  Post(
+      title: "lijinshanmx",
+      author: "2019-02-18",
+      imageUrl:
+          "https://ws1.sinaimg.cn/large/0065oQSqly1g0ajj4h6ndj30sg11xdmj.jpg"),
+  Post(
+      title: "lijinshanmx",
+      author: "2019-01-03",
+      imageUrl:
+          "https://ws1.sinaimg.cn/large/0065oQSqly1fytdr77urlj30sg10najf.jpg"),
+  Post(
+      title: "lijinshanmx",
+      author: "2018-12-28",
+      imageUrl:
+          "https://ws1.sinaimg.cn/large/0065oQSqly1fymj13tnjmj30r60zf79k.jpg"),
+  Post(
+      title: "lijinshanmx",
+      author: "2018-12-13",
+      imageUrl:
+          "https://ws1.sinaimg.cn/large/0065oQSqgy1fy58bi1wlgj30sg10hguu.jpg"),
+  Post(
+      title: "lijinshanmx",
+      author: "2018-11-28",
+      imageUrl:
+          "https://ws1.sinaimg.cn/large/0065oQSqgy1fxno2dvxusj30sf10nqcm.jpg"),
+  Post(
+      title: "lijinshanmx",
+      author: "2018-11-19",
+      imageUrl:
+          "https://ws1.sinaimg.cn/large/0065oQSqgy1fxd7vcz86nj30qo0ybqc1.jpg"),
+  Post(
+      title: "lijinshanmx",
+      author: "2018-11-06",
+      imageUrl:
+          "https://ws1.sinaimg.cn/large/0065oQSqgy1fwyf0wr8hhj30ie0nhq6p.jpg"),
+  Post(
+      title: "lijinshanmx",
+      author: "2018-10-22",
+      imageUrl:
+          "https://ws1.sinaimg.cn/large/0065oQSqgy1fwgzx8n1syj30sg15h7ew.jpg"),
+  Post(
+      title: "lijinshanmx",
+      author: "2018-10-15",
+      imageUrl:
+          "https://ws1.sinaimg.cn/large/0065oQSqly1fw8wzdua6rj30sg0yc7gp.jpg"),
+  Post(
+      title: "lijinshanmx",
+      author: "2018-10-08",
+      imageUrl:
+          "https://ws1.sinaimg.cn/large/0065oQSqly1fw0vdlg6xcj30j60mzdk7.jpg"),
+  Post(
+      title: "lijinshanmx",
+      author: "2018-09-19",
+      imageUrl:
+          "https://ws1.sinaimg.cn/large/0065oQSqly1fvexaq313uj30qo0wldr4.jpg"),
+  Post(
+      title: "lijinshanmx",
+      author: "2018-09-11",
+      imageUrl:
+          "https://ws1.sinaimg.cn/large/0065oQSqly1fv5n6daacqj30sg10f1dw.jpg"),
+  Post(
+      title: "lijinshanmx",
+      author: "2018-08-27",
+      imageUrl:
+          "https://ws1.sinaimg.cn/large/0065oQSqly1fuo54a6p0uj30sg0zdqnf.jpg"),
+  Post(
+      title: "lijinshanmx",
+      author: "2018-08-21",
+      imageUrl:
+          "https://ws1.sinaimg.cn/large/0065oQSqly1fuh5fsvlqcj30sg10onjk.jpg"),
+  Post(
+      title: "lijinshan",
+      author: "2018-08-16",
+      imageUrl:
+          "https://ws1.sinaimg.cn/large/0065oQSqly1fubd0blrbuj30ia0qp0yi.jpg"),
+  Post(
+      title: "lijinshan",
+      author: "2018-08-13",
+      imageUrl:
+          "https://ww1.sinaimg.cn/large/0065oQSqly1fu7xueh1gbj30hs0uwtgb.jpg"),
+  Post(
+      title: "lijinshanmx",
+      author: "2018-08-09",
+      imageUrl:
+          "https://ww1.sinaimg.cn/large/0065oQSqgy1fu39hosiwoj30j60qyq96.jpg"),
+  Post(
+      title: "lijinshan",
+      author: "2018-08-06",
+      imageUrl:
+          "https://ww1.sinaimg.cn/large/0065oQSqly1ftzsj15hgvj30sg15hkbw.jpg")
+];
