d2f6d8f237e3efd23df14ab892f37a5485143c14
xh <xhconceit@163.com>
2019-05-27 23:24:27 +0800

    RichText:多样式文本  TextSpan:小段文本小部件

--- lib/demo/basic_demo.dart
+++ lib/demo/basic_demo.dart
@@ -1,7 +1,37 @@
 import 'package:flutter/material.dart';
 
-class BasicDemo extends StatelessWidget{
-  final TextStyle _TextStyle = TextStyle(//文本样式小部件
+class BasicDemo extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return RichTextDemo();
+  }
+}
+
+class RichTextDemo extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return RichText(
+      //多样式文本小部件
+      text: TextSpan(
+          //小段文本小部件
+          text: "Hello",
+          style: TextStyle(
+              //
+              color: Colors.deepPurpleAccent, //文字颜色
+              fontStyle: FontStyle.italic, //字体
+              fontWeight: FontWeight.w100),
+          children: [
+            TextSpan(
+                text: "world",
+                style: TextStyle(fontSize: 17.0, color: Colors.grey))
+          ]),
+    );
+  }
+}
+
+class TextDemo extends StatelessWidget {
+  final TextStyle _TextStyle = TextStyle(
+    //文本样式小部件
     fontSize: 16.0,
   );
 
@@ -9,10 +39,11 @@ class BasicDemo extends StatelessWidget{
   final String _title = "我的团长, 我的团";
 
   @override
-  Widget build(BuildContext context){
-    return Text(//文本小部件
+  Widget build(BuildContext context) {
+    return Text(
+      //文本小部件
       "《 $_title 》 —— $_author 此狗昔日沦落为奴中之婢，今日得势如帝国列强，咬了对街爱新觉罗氏，西门朱氏，左邻蒋氏，连右舍老孟家的小猪崽子的左蹄髈也几被重伤不治…",
-      textAlign: TextAlign.left,//文本对齐方式
+      textAlign: TextAlign.left, //文本对齐方式
       style: _TextStyle,
       maxLines: 3,
       overflow: TextOverflow.ellipsis,
