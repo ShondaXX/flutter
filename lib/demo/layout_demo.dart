12b3481d847bcc7138537f577af4476317cb8dd4
xh <xhconceit@163.com>
2019-05-29 00:15:10 +0800

    Stack:叠加小部件 Positioned:定位小部件

--- lib/demo/layout_demo.dart
+++ lib/demo/layout_demo.dart
@@ -1,27 +1,57 @@
 import 'package:flutter/material.dart';
 
-class LayoutDemo extends StatelessWidget{
+class LayoutDemo extends StatelessWidget {
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return Container(
-      child: Row(//横排小部件
-        mainAxisAlignment: MainAxisAlignment.center,//主轴
+      child: Row(
+        //横排小部件
+        mainAxisAlignment: MainAxisAlignment.center, //主轴
         children: <Widget>[
-          SizedBox(//小盒子小部件
-            width: 200.0,
-            height: 300.0,
-            child: Container(//背景小部件
-              alignment: Alignment(1.0, -1.0),//对齐位置
-              decoration: BoxDecoration(
-                color: Color.fromRGBO(3, 54, 255, 1.0),
-                borderRadius: BorderRadius.circular(8.0),
+          Stack(//叠加小部件
+             alignment: Alignment.bottomCenter,
+            children: <Widget>[
+              SizedBox(
+                //小盒子小部件
+                width: 200.0,
+                height: 300.0,
+                child: Container(
+                  //背景小部件
+                  alignment: Alignment(1.0, -1.0), //对齐位置
+                  decoration: BoxDecoration(
+                    color: Color.fromRGBO(3, 54, 255, 1.0),
+                    borderRadius: BorderRadius.circular(8.0),
+                    gradient: RadialGradient(//镜像渐变小部件
+                      colors: [
+                        Color.fromRGBO(7, 102, 255, 1.0),
+                        Color.fromRGBO(3, 54, 255, 1.0 )
+                      ]
+                    )
+                  ),
+                  child: Icon(
+                    //图标小部件
+                    Icons.ac_unit,
+                    color: Colors.white,
+                    size: 32.0,
+                  ),
+                ),
               ),
-              child: Icon(//图标小部件
-                Icons.ac_unit,
-                color: Colors.white,
-                size: 32.0,
+              SizedBox(
+                child: IconBadge(
+                  Icons.access_time,
+                  size: 64.0,
+                ),
               ),
-            ),
+              Positioned(//定位小部件
+                right: 20.0,
+                 top: 20.0,
+                child: Icon(
+                  Icons.accessibility_new,
+                  color: Colors.white,
+                  size: 32.0,
+                ),
+              )
+            ],
           )
         ],
       ),
@@ -29,26 +59,25 @@ class LayoutDemo extends StatelessWidget{
   }
 }
 
-class IconBadge extends StatelessWidget{
+class IconBadge extends StatelessWidget {
   final IconData icon;
   final double size;
 
-  IconBadge(this.icon,{
-    this.size = 32.0
-  });
+  IconBadge(this.icon, {this.size = 32.0});
 
   @override
-  Widget build(BuildContext context){
-    return Container(//盒子小部件
-      child: Icon(//图标小部件
+  Widget build(BuildContext context) {
+    return Container(
+      //盒子小部件
+      child: Icon(
+        //图标小部件
         icon,
         size: size,
         color: Colors.white,
-        ),
-        width: size + 60,
-        height: size + 60,
-        color: Color.fromRGBO(3, 54, 255, 1.0),
+      ),
+      width: size + 60,
+      height: size + 60,
+      color: Color.fromRGBO(3, 54, 255, 1.0),
     );
   }
-
 }
