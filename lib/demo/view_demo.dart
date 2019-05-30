ae60547608e284a9bc8702a0de5d7edaa0b82442
xh <xhconceit@163.com>
2019-05-31 00:23:26 +0800

    PageController:控制滚动页面视图默认显示小部件

--- lib/demo/view_demo.dart
+++ lib/demo/view_demo.dart
@@ -1,48 +1,69 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/painting.dart';
 
-class ViewDemo extends StatelessWidget{
+class ViewDemo extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return PageView(//页面视图小部件
+    return PageViewDemo();
+  }
+}
+
+class PageViewDemo extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return PageView(
+      //页面视图小部件
       // pageSnapping: false,//是否自动滑动
       // reverse: true,//页面视图倒置
       // scrollDirection: Axis.vertical,//页面滑动方向
-      onPageChanged: (currentPage){//页面变换视图事件
+      onPageChanged: (currentPage) {
+        //页面变换视图事件
         debugPrint("Page: $currentPage");
       },
+      controller: PageController(
+        //控制页面视图显示小部件
+        initialPage: 1, //默认显示页面
+        keepPage: false, //是否记录页面滚动页面
+        viewportFraction: 0.8, //页面大小
+      ),
       children: <Widget>[
-        Container(//盒子小部件
+        Container(
+          //盒子小部件
           color: Colors.brown[900],
-          alignment: Alignment(0.0, 0.0),//对齐
-          child: Text(//文本小部件
+          alignment: Alignment(0.0, 0.0), //对齐
+          child: Text(
+            //文本小部件
             "ONE",
-            style: TextStyle(//文本样式  
-              fontSize: 32.0,
-              color: Colors.white
-            ),
+            style: TextStyle(
+                //文本样式
+                fontSize: 32.0,
+                color: Colors.white),
           ),
         ),
-        Container(//盒子小部件
+        Container(
+          //盒子小部件
           color: Colors.blue[900],
-          alignment: Alignment(0.0, 0.0),//对齐
-          child: Text(//文本小部件
+          alignment: Alignment(0.0, 0.0), //对齐
+          child: Text(
+            //文本小部件
             "TWO",
-            style: TextStyle(//文本样式  
-              fontSize: 32.0,
-              color: Colors.white
-            ),
+            style: TextStyle(
+                //文本样式
+                fontSize: 32.0,
+                color: Colors.white),
           ),
         ),
-        Container(//盒子小部件
+        Container(
+          //盒子小部件
           color: Colors.blueGrey[900],
-          alignment: Alignment(0.0, 0.0),//对齐
-          child: Text(//文本小部件
+          alignment: Alignment(0.0, 0.0), //对齐
+          child: Text(
+            //文本小部件
             "THREE",
-            style: TextStyle(//文本样式  
-              fontSize: 32.0,
-              color: Colors.white
-            ),
+            style: TextStyle(
+                //文本样式
+                fontSize: 32.0,
+                color: Colors.white),
           ),
         )
       ],
