056851480854c06f6b3ca8758da2961bfd54eee0
xh <xhconceit@163.com>
2019-05-31 23:18:24 +0800

    SliverSafeArea:安全区小部件  SliverPadding:滚动视图内边距小部件

--- lib/demo/sliver_demo.dart
+++ lib/demo/sliver_demo.dart
@@ -1,33 +1,47 @@
 import 'package:flutter/material.dart';
 import '../model/post.dart';
 
-class SliverDemo extends StatelessWidget{
+class SliverDemo extends StatelessWidget {
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return Scaffold(
-      body: CustomScrollView(//滚动视图小部件
+      body: CustomScrollView(
+        //滚动视图小部件
         slivers: <Widget>[
-          SliverGrid(//网格视图小部件
-            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-              crossAxisCount: 2,
-              crossAxisSpacing: 8.0,
-              mainAxisSpacing: 8.0,
-              childAspectRatio: 1.0,
-            ),
-            delegate: SliverChildBuilderDelegate(
-              (BuildContext context,int index){
-                return Container(
-                  child: Image.network(
-                    posts[index].imageUrl,
-                    fit: BoxFit.cover,
-                  ),
-                );
-              },
-              childCount: posts.length,
-
+          SliverSafeArea(//安全区小部件
+            sliver: SliverPadding(
+              //滚动视图内边距小部件
+              padding: EdgeInsets.all(8.0),
+              sliver: SliverGridDemo(),
             ),
           )
         ],
+      ),
+    );
+  }
+}
+
+class SliverGridDemo extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SliverGrid(
+      //网格视图小部件
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 2,
+        crossAxisSpacing: 8.0,
+        mainAxisSpacing: 8.0,
+        childAspectRatio: 1.0,
+      ),
+      delegate: SliverChildBuilderDelegate(
+        (BuildContext context, int index) {
+          return Container(
+            child: Image.network(
+              posts[index].imageUrl,
+              fit: BoxFit.cover,
+            ),
+          );
+        },
+        childCount: posts.length,
       ),
     );
   }
