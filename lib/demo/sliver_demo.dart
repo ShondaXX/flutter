4705c12411064ebad96f888fb1553868fb7c81c6
xh <xhconceit@163.com>
2019-05-31 23:28:10 +0800

    SliverList:列表视图小部件

--- lib/demo/sliver_demo.dart
+++ lib/demo/sliver_demo.dart
@@ -8,14 +8,71 @@ class SliverDemo extends StatelessWidget {
       body: CustomScrollView(
         //滚动视图小部件
         slivers: <Widget>[
-          SliverSafeArea(//安全区小部件
+          SliverSafeArea(
+            //安全区小部件
             sliver: SliverPadding(
               //滚动视图内边距小部件
               padding: EdgeInsets.all(8.0),
-              sliver: SliverGridDemo(),
+              sliver: SliverListDemo(),
             ),
           )
         ],
+      ),
+    );
+  }
+}
+
+class SliverListDemo extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SliverList(
+      //列表视图小部件
+      delegate: SliverChildBuilderDelegate(
+        (BuildContext context, int index) {
+          return Padding(
+            //内边距小部件
+            padding: EdgeInsets.only(bottom: 32.0),
+            child: Material(
+                borderRadius: BorderRadius.circular(12.0),
+                elevation: 14.0,
+                shadowColor: Colors.grey.withOpacity(0.5),
+                child: Stack(
+                  children: <Widget>[
+                    AspectRatio(
+                      aspectRatio: 9 / 16,
+                      child: Image.network(
+                        posts[index].imageUrl,
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                    Positioned(
+                      top: 32.0,
+                      left: 32.0,
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Text(
+                            posts[index].title,
+                            style: TextStyle(
+                              fontSize: 20.0,
+                              color: Colors.white
+                            ),
+                          ),
+                          Text(
+                            posts[index].author,
+                            style: TextStyle(
+                              fontSize: 40.0,
+                              color: Colors.white
+                            ),
+                          ),
+                        ],
+                      ),
+                    )
+                  ],
+                )),
+          );
+        },
+        childCount: posts.length,
       ),
     );
   }
