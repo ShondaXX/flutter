c61d6b6593ffa94b2746fe15de87ab46c028249e
xh <xhconceit@163.com>
2019-06-02 23:17:12 +0800

    FloatingActionButtonLocation    CircularNotchedRectangle

--- lib/demo/material_components.dart
+++ lib/demo/material_components.dart
@@ -25,10 +25,10 @@ class FloatingActionButtonDemo extends StatelessWidget {
     onPressed: (){},
     child: Icon(Icons.add),
     elevation:  0.0,
-    backgroundColor: Colors.black87,
-    shape: BeveledRectangleBorder(//正方形
-      borderRadius: BorderRadius.circular(30.0)
-    ),
+    // backgroundColor: Colors.black87,
+    // shape: BeveledRectangleBorder(//正方形
+    //   borderRadius: BorderRadius.circular(30.0)
+    // ),
   );
 
   final _floatingActionButtonExtended = FloatingActionButton.extended(//
@@ -44,7 +44,14 @@ class FloatingActionButtonDemo extends StatelessWidget {
         title: Text("FloatingActionButtonDemo"),
         elevation: 0.0,
       ),
-      floatingActionButton: _floatingActionButtonExtended,
+      floatingActionButton: _floatingActionButton,
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,//按钮和工具栏叠合一块
+      bottomNavigationBar: BottomAppBar(//底部工具栏小部件
+        child: Container(
+          height: 80.0,
+        ),
+        shape: CircularNotchedRectangle(),//圆形缺口
+      ),
     );
   }
 }
