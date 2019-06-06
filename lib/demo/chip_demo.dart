47cb08d3a323c70a2862acedc7da573a232f9b27
xh <xhconceit@163.com>
2019-06-07 00:13:27 +0800

    Divider:分隔符小部件    ActionChip:动作小碎片小部件

--- lib/demo/chip_demo.dart
+++ lib/demo/chip_demo.dart
@@ -6,11 +6,19 @@ class ChipDemo extends StatefulWidget {
 }
 
 class _ChipDemoState extends State<ChipDemo> {
+  List<String> _tags = [
+    "Apple",
+    "Banana",
+    "Lemon",
+  ];
+
+  String _action = "Nothing";
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("_WidgetDemo"),
+        title: Text("Chip"),
         elevation: 0.0,
       ),
       body: Container(
@@ -18,38 +26,112 @@ class _ChipDemoState extends State<ChipDemo> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
+            Wrap(//自动换行小部件
+              spacing: 8.0,//小部件间隔
+              runSpacing: 8.0,//行间隔
               children: <Widget>[
-                Chip(//小碎片小部件
+                Chip(
+                  //小碎片小部件
                   label: Text("Life"),
                 ),
-                SizedBox(
-                  width: 8.0,
-                ),
-                Chip(//小碎片小部件
+                Chip(
+                  //小碎片小部件
                   label: Text("Sunset"),
                   backgroundColor: Colors.orange,
                   avatar: CircleAvatar(
                     backgroundColor: Colors.grey,
+                    child: Text("王"),
+                  ),
+                ),
+                Chip(
+                  //小碎片小部件
+                  label: Text("Sunset"),
+                  backgroundColor: Colors.orange,
+                  avatar: CircleAvatar(
+                    //圆形头像小部件
+                    backgroundImage: NetworkImage(
+                        "http://ww1.sinaimg.cn/large/0065oQSqly1g2pquqlp0nj30n00yiq8u.jpg"),
+                    child: Text("思"),
+                  ),
+                ),
+                Chip(
+                  //小碎片小部件
+                  label: Text("Sunset"),
+                  backgroundColor: Colors.orange,
+                  avatar: CircleAvatar(
+                    //圆形头像小部件
+                    backgroundImage: NetworkImage(
+                        "http://ww1.sinaimg.cn/large/0065oQSqly1g2pquqlp0nj30n00yiq8u.jpg"),
                     child: Text("婷"),
                   ),
                 ),
-                
-                Chip(//小碎片小部件
-                  label: Text("Sunset"),
-                  backgroundColor: Colors.orange,
-                  avatar: CircleAvatar(//圆形头像小部件
-                    backgroundImage: NetworkImage(
-                      "http://ww1.sinaimg.cn/large/0065oQSqly1g2pquqlp0nj30n00yiq8u.jpg"
-                    ),
-                    child: Text("婷"),
-                  ),
-                )
+                Chip(
+                  label: Text("City"),
+                  onDeleted: (){},
+                  deleteIcon: Icon(Icons.delete),//删除图标
+                  deleteIconColor: Colors.redAccent,//删除图标颜色
+                  deleteButtonTooltipMessage: "Remove this tag",//长按提示
+                ),
+                Divider(//分隔符小部件
+                  color: Colors.grey,
+                  height: 32.0,
+                  indent: 100.0,
+                ),
+                Wrap(
+                  spacing: 8.0,
+                  children: _tags.map(
+                    (tag){
+                      return Chip(
+                        label: Text(tag),
+                        onDeleted: (){
+                          setState(() {
+                           _tags.remove(tag);//删除当前 chip
+                          });
+                        },
+                      );
+                    }
+                  ).toList(),
+                ),
+                Divider(//分隔符小部件
+                  color: Colors.grey,
+                  height: 32.0,
+                  indent: 100.0,
+                ),
+                Container(
+                  width: double.infinity,
+                  child: Text("ActionChip $_action"),
+                ),
+                Wrap(
+                  spacing: 8.0,
+                  children: _tags.map(
+                    (tag){
+                      return ActionChip(//动作小碎片小部件
+                        label: Text(tag),
+                        onPressed: (){
+                          setState(() {
+                           _action = tag; 
+                          });
+                        },
+                      );
+                    }
+                  ).toList(),
+                ),
               ],
             )
           ],
         ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.restore),
+        onPressed: (){
+          setState(() {
+           _tags = [
+             "Apple",
+             "Banana",
+             "Lemons"
+           ];
+          });
+        },
       ),
     );
   }
