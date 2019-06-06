9978c4198abf2f19f74af8156eee499fe292c00b
xh <xhconceit@163.com>
2019-06-07 01:45:37 +0800

    Card:卡片小部件

---
+++ lib/demo/card_demo.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import '../model/post.dart';
+class CardDemo extends StatefulWidget {
+  @override
+  _ CardDemoState createState() => _CardDemoState();
+}
+
+class _CardDemoState extends State<CardDemo> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("_WidgetDemo"),
+        elevation: 0.0,
+      ),
+      body: Container(
+        padding: EdgeInsets.all(16.0),
+        child: ListView(
+          children: posts.map(
+            (post){
+              return Card(
+                child: Column(
+                  children: <Widget>[
+                    AspectRatio(
+                      aspectRatio: 16/9,
+                      child: Image.network(
+                        post.imageUrl,
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                    ListTile(
+                      leading: CircleAvatar(
+                        backgroundImage: NetworkImage(post.imageUrl),
+                      ),
+                      title: Text(post.title),
+                      subtitle: Text(post.author),
+                    ),
+                  ],
+                ),
+              );
+            }
+          ).toList()
+        ),
+      ),
+    );
+  }
+}
--- lib/demo/material_components.dart
+++ lib/demo/material_components.dart
@@ -15,6 +15,7 @@ import './expansion_panel_demo.dart';
 import './chip_demo.dart';
 import './data_table_demo.dart';
 import './paginated_data_table_demo.dart';
+import './card_demo.dart';
 
 class MaterialComponents extends StatelessWidget {
   @override
@@ -26,6 +27,10 @@ class MaterialComponents extends StatelessWidget {
       ),
       body: ListView(
         children: <Widget>[
+          ListItem(
+            title: "CardDemo",
+            page: CardDemo(),
+          ),
           ListItem(
             title: "PaginatedDataTableDemo",
             page: PaginatedDataTableDemo(),
